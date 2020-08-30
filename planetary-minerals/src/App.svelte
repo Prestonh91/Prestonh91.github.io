456a179fa8517ed4efb850778f37963ade1c7c2f
Preston Higgins <preston.higgins91@gmail.com>
2020-08-30 14:50:47 -0600

    Init

---
+++ planetary-minerals/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<main class="uk-height-viewport">
+	<section class="uk-card uk-card-body">
+	</section>
+</main>
+
+<style>
+</style>
---
+++ planetary-minerals/src/resources/materials.js
@@ -0,0 +1,191 @@
+export const AList = [
+	{
+		id: 0,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad I',
+		materials: [
+			{
+				name: 'Noble Gas',
+				quality: 'Poor',
+			},
+			{
+				name: 'Oxygen Isotopes',
+				quality: 'Medium',
+			}
+		],
+	},
+	{
+		id: 1,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad II',
+		materials: [
+			{
+				name: 'Gleaming Alloy',
+				quality: 'Medium'
+			},
+			{
+				name: 'Crystal Compound',
+				quality: 'Perfect',
+			},
+			{
+				name: 'Construction Blocks',
+				quality: 'Poor'
+			}
+		],
+	},
+	{
+		id: 2,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad III',
+		materials: [
+		],
+	},
+	{
+		id: 3,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Usad IV',
+		materials: [
+			{
+				name: 'Glossy Compound',
+				quality: 'Perfect',
+			},
+			{
+				name: 'Noble Gas',
+				quality: 'Rich'
+			},
+			{
+				name: 'Base Metals',
+				quality: 'Medium',
+			},
+			{
+				name: 'Ionic Solutions',
+				quality: 'Rich'
+			}
+		],
+	},
+	{
+		id: 4,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad V',
+		materials: [
+			{
+				name: 'Glossy Compoud',
+				quality: 'Perfect'
+			},
+			{
+				name: 'Noble Gas',
+				quality: 'Medium'
+			},
+			{
+				name: 'Base Metals',
+				quality: 'Poor'
+			},
+			{
+				name: 'Ionic Solutions',
+				quality: 'Rich'
+			},
+		],
+	},
+	{
+		id: 5,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad VI',
+		materials: [
+			{
+				name: 'Sheen Compound',
+				quality: 'Medium',
+			},
+			{
+				name: 'Glossy Compound',
+				quality: 'Medium'
+			},
+			{
+				name: 'Noble Gas',
+				quality: 'Rich',
+			},
+			{
+				name: 'Ionic Solutions',
+				quality: 'Rich'
+			}
+		],
+	},
+	{
+		id: 6,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad VII',
+		materials: [
+			{
+				name: 'Lustering Alloy',
+				quality: 'Medium',
+			},
+			{
+				name: 'Fiber Composite',
+				quality: 'Medium'
+			},
+			{
+				name: 'Reactive Gas',
+				quality: 'Rich'
+			},
+			{
+				name: 'Noble Gas',
+				quality: 'Medium'
+			}
+		],
+	},
+	{
+		id: 7,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad VIII',
+		materials: [
+			{
+				name: 'Heavy Metals',
+				quality: 'Poor',
+			},
+			{
+				name: 'Coolant',
+				quality: 'Poor'
+			},
+			{
+				name: 'Suspended Plasma',
+				quality: 'Poor'
+			},
+		],
+	},
+	{
+		id: 8,
+		region: 'Devoid',
+		cluster: 'Kabo',
+		system: 'Ussad',
+		planet: 'Ussad IX',
+		materials: [
+			{
+				name: 'Lucent Compound',
+				quality: "Medium"
+			},
+			{
+				name: 'Reactive Gas',
+				quality: 'Medium'
+			},
+			{
+				name: 'Contruction Blocks',
+				quality: 'Medium',
+			},
+		],
+	},
+]
