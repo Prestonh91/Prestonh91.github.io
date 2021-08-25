9d2ba724a7ba212657f61c7936dc86a8bd4fdd86
Preston Higgins <phiggins@porch.software>
2021-08-24 23:28:43 -0600

    Quest service

---
+++ gamify-life/src/app/services/quest/quest.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Quest } from 'src/app/classes/Quest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestService {
+	private questUrl: string = 'quests/'
+
+	constructor(private fireDB: AngularFireDatabase) { }
+
+	private getQuestAuthorUrl(authorUid: string) {
+		return `${this.questUrl}/${authorUid}`
+	}
+
+	createNewQuest(quest: Quest) {
+		var questDbRef = this.fireDB.database.ref(this.getQuestAuthorUrl(quest.author || '')).push()
+		quest.uid = questDbRef.key
+		return questDbRef.set(quest.prepareForSave())
+	}
+
+	getQuests(guardianUid: string) {
+		return this.fireDB.object(this.getQuestAuthorUrl(guardianUid)).valueChanges()
+	}
+}
