901c7fb2401d195a53071262376aa8360679cee3
Preston Higgins <phiggins@porch.software>
2021-10-09 21:55:10 -0600

    WIP: Purchase perks

---
+++ gamify-life/src/app/components/off-canvas/perk-redeem/perk-redeem.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnChanges, OnInit} from '@angular/core';
+import { Ward } from 'src/app/classes';
+import { Perk } from 'src/app/classes/Perk';
+
+@Component({
+  selector: 'perk-redeem',
+  templateUrl: './perk-redeem.component.html',
+  styleUrls: ['./perk-redeem.component.scss']
+})
+export class PerkRedeemComponent implements OnInit, OnChanges {
+	@Input() perks: Array<Perk> = new Array();
+	@Input() ward: Ward | null = null;
+
+	public perksToRedeem:  Array<Perk> = new Array();
+	public perkRedeemAmounts: any = {};
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	ngOnChanges(): void {
+			if (this.perks.length) {
+			this.perkRedeemAmounts = {}
+			this.perksToRedeem = []
+			for (let p of this.perks) {
+				this.perkRedeemAmounts[p.uid] = 1
+				this.perksToRedeem.push(new Perk(p))
+			}
+		}
+	}
+
+	addToPerkCount(perk: Perk) {
+		let perkCount = this.perkRedeemAmounts[perk.uid]
+
+		// Only add on if the perk has enough durability
+		if ((perkCount + 1) <= perk.durability) {
+			this.perkRedeemAmounts[perk.uid] += 1
+		}
+	}
+
+	removeFromPerkCount(perk: Perk) {
+		this.perkRedeemAmounts[perk.uid] -= 1
+	}
+
+	removePerkFromRedeemList(perk: Perk) {
+		var i = this.perksToRedeem.findIndex(x => x.uid === perk.uid)
+
+		if (i > -1) {
+			this.perksToRedeem.splice(i, 1)
+		}
+	}
+}
